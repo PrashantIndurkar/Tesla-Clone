be436259c1e1941d4f6fdf44c75fe7ba3c9c823f
PrashantIndurkar <prashantri2000@gmail.com>
2022-01-17 17:03:47 +0530

    Created Navbar with no function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+// import about from './components/about';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +17,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+
 serviceWorker.unregister();
