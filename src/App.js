a6483d85483c1ba245f49216e5d570a6d4fcecd7
PrashantIndurkar <prashantri2000@gmail.com>
2022-01-24 21:00:36 +0530

    added car and tesla account page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,76 @@
-import React,{useState} from "react";
-import "./App.css";
-import Header from "./Header";
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Header from './Header'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom'
-import Menu from "./Menu"
-import HeaderBlock from "./HeaderBlock";
-import Login from "./Login"
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/counter/userSlice";
-import Signup from "./Signup";
+import Menu from './Menu'
+import HeaderBlock from './HeaderBlock'
+import Login from './Login'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/counter/userSlice'
+import Signup from './Signup'
+import TeslaAccount from './TeslaAccount'
+import { auth } from './firebase'
+
 function App() {
   const user = useSelector(selectUser)
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-    return(
-      <Router>
-      <div className="app">
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // User is signed in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        )
+      } else {
+        // User is signed out
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
+
+  return (
+    <Router>
+      <div className='app'>
         <Switch>
-          <Route exact path="/">
-            <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+          <Route exact path='/'>
+            <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
             {isMenuOpen && <Menu />}
             <HeaderBlock />
-            </Route>
-        <Route exact path="/login">
-        {user ? <Redirect to="/teslaaccount"/>: <Login/> }
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-
+          </Route>
+          <Route exact path='/login'>
+            {user ? <Redirect to='/teslaaccount' /> : <Login />}
+          </Route>
+          <Route exact path='/signup'>
+            <Signup />
+          </Route>
+          <Route exact path='/teslaaccount'>
+            {!user ? (
+              <Redirect to='/login' />
+            ) : (
+              <>
+                <TeslaAccount
+                  isMenuOpen={isMenuOpen}
+                  setIsMenuOpen={setIsMenuOpen}
+                />
+                {isMenuOpen && <Menu />}
+              </>
+            )}
+          </Route>
         </Switch>
       </div>
-      </Router>
-
-    )
-
+    </Router>
+  )
 }
 
-export default App;
+export default App
